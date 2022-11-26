6fb4836f74ea40a4b4508fc5ec9d703ef5b9f498
Okechukwu <okei.muokwugwo@outlook.com>
2022-11-26 20:05:17 +0100

    Create Coins.js component

--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,32 +1,61 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { FiChevronLeft } from 'react-icons/fi';
+import { BsGear } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 import { getCoinsAPI } from '../redux/coins/coinsSlice';
 import Coin from './Coin/Coin';
+import '../App.css';
 
 const Coins = () => {
+  const [input, setInput] = useState('');
   const coins = useSelector((state) => state.coins);
-  console.log(coins);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getCoinsAPI());
   }, [dispatch]);
 
+  const filterCoins = coins.filter((coin) => (
+    coin.name.toLowerCase().includes(input.toLocaleLowerCase())
+  ));
+
+  const searchCoins = (e) => setInput(e.target.value);
+
   return (
     <>
-      <ul className="coins-card">
-        {coins.coins.map((coin) => (
-          <Coin
-            key={coin.id}
-            img={coin.icon}
-            id={coin.id}
-            symbol={coin.symbol}
-            rank={coin.rank}
-            name={coin.name}
-            price={coin.price}
+      <div>
+        <nav className="search-wrapper">
+          <Link
+            className="home-link"
+            to="/"
+          >
+            <FiChevronLeft />
+          </Link>
+          <input
+            type="text"
+            placeholder="Search Coins"
+            value={input}
+            onChange={searchCoins}
           />
-        ))}
-      </ul>
+          <BsGear
+            className="gear-link"
+          />
+        </nav>
+        <ul className="coins-card">
+          {filterCoins.map((coin) => (
+            <Coin
+              key={coin.id}
+              img={coin.icon}
+              id={coin.id}
+              symbol={coin.symbol}
+              rank={coin.rank}
+              name={coin.name}
+              price={coin.price}
+            />
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
